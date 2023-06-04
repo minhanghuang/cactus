ba615e097108c6d2f0301f62b2a1ac7b0fe5a141
minhanghuang <job@minhang.me>
2023-06-04 14:44:00 +0800

    feat: 增加注册类成员变量宏

--- include/cactus/macros.h
+++ include/cactus/macros.h
@@ -1,9 +1,28 @@
 #ifndef CACTUS_MACROS_H_
 #define CACTUS_MACROS_H_
+
 #include <iostream>
 #include <memory>
 #include <mutex>
 #include <type_traits>
+
+#define CACTUS_REGISTER_MEMBER_BASIC_TYPE(type, var, value) \
+ private:                                                   \
+  type var##_ = value;                                      \
+                                                            \
+ public:                                                    \
+  type var() const noexcept { return var##_; }              \
+  void set_##var(const type var) noexcept { var##_ = var; }
+
+#define CACTUS_REGISTER_MEMBER_COMPLEX_TYPE(type, var)       \
+ private:                                                    \
+  type var##_;                                               \
+                                                             \
+ public:                                                     \
+  const type& var() const noexcept { return var##_; }        \
+  type* mutable_##var() noexcept { return &var##_; }         \
+  void set_##var(const type& var) noexcept { var##_ = var; } \
+  void set_##var(type&& var) noexcept { var##_ = std::move(var); }
 
 #define CACTUS_DEFINE_TYPE_TRAIT(name, func)              \
   template <typename T>                                   \
