997b9c8542a163ef351617c7f57492f0aaf41b2e
minhanghuang <job@minhang.me>
2023-06-06 15:46:07 +0800

    fix: 修复测试脚本在Ubuntu16运行失败问题

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,8 +8,9 @@ function test() {
   echo "---------------------------------------------------------------"
   echo "------------------------- run all test ------------------------"
   echo "---------------------------------------------------------------"
-  cmake -B build -DBUILD_SHARED_LIBS=ON -DBUILD_CACTUS_TEST=ON -DCMAKE_BUILD_TYPE=Debug .
-  cmake --build build -j6
+  mkdir -p build && cd build
+  cmake -DBUILD_SHARED_LIBS=ON -DBUILD_CACTUS_TEST=ON -DCMAKE_BUILD_TYPE=Debug ..
+  make -j4
   local bin_path="$CURRENT_PATH/../build/tests"
   for file in $bin_path/*
   do
